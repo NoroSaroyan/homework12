ff5e7c0e9192d7bc14800b842c5d49af5cca877d
noriksaroyan <noriksaroyan@gmail.com>
2021-10-10 18:52:32 +0300

    ready to go

---
+++ src/main/java/ru/gb/entity/Cart.java
@@ -0,0 +1,26 @@
+package ru.gb.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "carts")
+
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class Cart {
+    @Id
+    @GeneratedValue
+    protected Long id;
+    protected Long countProduct;
+    @ManyToOne
+    protected Product product;
+}
